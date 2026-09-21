b1b600661ba6c832fbbce1e812831e05d5ddba9b
Erlend Tøssebro <erlend.tossebro@uis.no>
2026-09-21 09:32:24 +0200

    Skriver alle klokkeslett

--- kontrollstrukturer/nostet_for_lokke.py
+++ kontrollstrukturer/nostet_for_lokke.py
@@ -1,6 +1,8 @@
-for time in range(1, 7):
+for time in range(1, 24):
     print(time)
-    for minutt in range(1, 4):
+    for minutt in range(1, 60):
         print(f"{time}:{minutt}")
+        for sekund in range(1, 60):
+            print(f"{time}:{minutt}:{sekund}")
 
 # Antall ganger linje 4 kjører er 6*3 = 18
